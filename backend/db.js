79cb6e349f4789ad47a494367d4ccdbfd628372b
brunnoL <brunnoL.@email.com>
2025-05-08 16:15:03 -0300

    :card_file_box: DB atualizado

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
+  user: 'brunno',
   password: 'brunno33',
-  database: 'aluguel_jogos'
+  database: 'aluguel_de_jogos',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 module.exports = pool.promise();
